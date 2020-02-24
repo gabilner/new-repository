64f7514e9aead17ee64269e2f25fb401e078453a
Gavriel Magilner-גבריאל מגילנר <GabiM@bezeqint.co.il>
2020-02-24 14:14:07 +0200

    ops assignment

--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
+
+sudo sed -i '3 a\192.168.100.10 server1' /etc/hosts
+
+### script for ex 5
+sudo chmod 777 /home/vagrant/.ssh/id_rsa
--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+
+sudo sed -i 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
+sudo apt-get install sshpass
+ssh-keygen -P "" -f /home/vagrant/.ssh/id_rsa
+sudo chmod 777 /home/vagrant/.ssh/id_rsa
+sleep 300&& sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub root@server2 &
+
